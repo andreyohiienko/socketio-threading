1aaee2cf436fb110bab39d8ac520c200345c67fd
Andrii <andreyohiienko@gmail.com>
2022-10-18 11:20:24 -0700

    Add logger and connect handler

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode="threading")
+socketio = SocketIO(app, async_mode="threading", engineio_logger=True)
 
 
 @app.route("/")
 def root():
     return "root"
+
+
+@socketio.on("connect")
+def connect():
+    print("connect")
 
 
 if __name__ == "__main__":
